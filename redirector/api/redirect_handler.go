c75144bafc6658945c1d9bbcf8fc90e48d727b98
mactavishz <maczhanchao@yahoo.com>
2025-06-19 17:10:13 +0200

    feat(redirector): send data to analytics service via grpc

--- redirector/api/redirect_handler.go
+++ redirector/api/redirect_handler.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"context"
 	"errors"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/mactavishz/kuerzen/analytics/grpc"
+	astore "github.com/mactavishz/kuerzen/store/analytics"
 	store "github.com/mactavishz/kuerzen/store/url"
 )
 
@@ -12,11 +16,13 @@ type Envelope map[string]any
 
 type RedirectHandler struct {
 	urlStore store.URLStore
+	client   *grpc.AnalyticsGRPCClient
 }
 
-func NewRedirectHandler(urlStore store.URLStore) *RedirectHandler {
+func NewRedirectHandler(urlStore store.URLStore, client *grpc.AnalyticsGRPCClient) *RedirectHandler {
 	return &RedirectHandler{
 		urlStore: urlStore,
+		client:   client,
 	}
 }
 
@@ -24,13 +30,34 @@ func (h *RedirectHandler) HandleRedirect(c *fiber.Ctx) error {
 	shortURL := c.Params("shortURL")
 	// TODO: handle caching of redirects
 	longURL, err := h.urlStore.GetLongURL(shortURL)
+	evt := &astore.URLRedirectEvent{
+		ServiceName: "redirector",
+		APIVer:      1,
+		Success:     false,
+		ShortURL:    shortURL,
+		LongURL:     longURL,
+		Timestamp:   time.Now(),
+	}
 	if err != nil {
 		if errors.Is(err, store.ErrShortURLNotFound) {
 			log.Printf("[HandleRedirect] short URL not found: %s\n", shortURL)
+			err = h.client.SendURLRedirectEvent(context.TODO(), evt)
+			if err != nil {
+				log.Printf("[HandleRedirect] failed to send anlytics: %v\n", err)
+			}
 			return c.Status(fiber.StatusNotFound).SendString("Not Found")
+		}
+		err = h.client.SendURLRedirectEvent(context.TODO(), evt)
+		if err != nil {
+			log.Printf("[HandleRedirect] failed to send anlytics: %v\n", err)
 		}
 		log.Printf("[HandleRedirect] failed to get long URL: %v\n", err)
 		return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+	}
+	evt.Success = true
+	err = h.client.SendURLRedirectEvent(context.TODO(), evt)
+	if err != nil {
+		log.Printf("[HandleRedirect] failed to send anlytics: %v\n", err)
 	}
 	// use 307 to prevent browsers from caching the redirect
 	return c.Redirect(longURL, fiber.StatusTemporaryRedirect)
--- redirector/main.go
+++ redirector/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/mactavishz/kuerzen/analytics/grpc"
 	"github.com/mactavishz/kuerzen/redirector/api"
 	"github.com/mactavishz/kuerzen/store/db"
 	"github.com/mactavishz/kuerzen/store/migrations"
@@ -27,7 +28,11 @@ func main() {
 	}
 	app := fiber.New()
 	urlStore := store.NewPostgresURLStore(pgDB)
-	handler := api.NewRedirectHandler(urlStore)
+	client, err := grpc.NewAnalyticsGRPCClient(os.Getenv("ANALYTICS_SERVICE_URL"))
+	if err != nil {
+		log.Fatalf("could not set up grpc client: %v", err)
+	}
+	handler := api.NewRedirectHandler(urlStore, client)
 
 	app.Get("/api/v1/url/:shortURL", handler.HandleRedirect)
 
