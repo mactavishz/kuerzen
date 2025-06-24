11fa591c97203440812e84d646b0cb9525e2ca9f
mactavishz <maczhanchao@yahoo.com>
2025-06-24 19:00:58 +0200

    feat(shortener): handle resource cleanup, use loadshed middleware, update air config

--- shortener/main.go
+++ shortener/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/timeout"
 	grpc "github.com/mactavishz/kuerzen/analytics/grpc"
+	"github.com/mactavishz/kuerzen/middleware/loadshed"
 	"github.com/mactavishz/kuerzen/shortener/api"
 	"github.com/mactavishz/kuerzen/store/db"
 	"github.com/mactavishz/kuerzen/store/migrations"
@@ -34,11 +37,31 @@ func main() {
 		BodyLimit: 1024 * 1024 * 1, // 1MB
 	})
 
+	loadshedMiddleware, err := loadshed.NewLoadSheddingMiddleware(loadshed.Config{
+		CPUThreshold: 0.9,
+		MemThreshold: 0.9,
+		Interval:     500 * time.Millisecond,
+	})
+	if err != nil {
+		log.Fatalf("could not set up load shedding middleware: %v", err)
+	}
+	app.Use(loadshedMiddleware)
+
 	urlStore := store.NewPostgresURLStore(pgDB)
 	client, err := grpc.NewAnalyticsGRPCClient(os.Getenv("ANALYTICS_SERVICE_URL"))
 	if err != nil {
 		log.Fatalf("could not set up grpc client: %v", err)
 	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Printf("Error closing grpc client: %v", err)
+		}
+	}()
+	defer func() {
+		if err := pgDB.Close(); err != nil {
+			log.Printf("Error closing database connection: %v", err)
+		}
+	}()
 	handler := api.NewShortenHandler(urlStore, client)
 
 	app.Post("/api/v1/url/shorten", timeout.NewWithContext(handler.HandleShortenURL, 3*time.Second))
@@ -50,6 +73,20 @@ func main() {
 	if len(port) == 0 {
 		port = DEFAULT_PORT
 	}
+	// Listen from a different goroutine
+	go func() {
+		if err := app.Listen(":" + port); err != nil {
+			log.Panic(err)
+		}
+	}()
 	log.Printf("Shortener service listening on port :%s", port)
-	app.Listen(":" + port)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c
+	log.Println("Gracefully shutting down...")
+	if err := app.Shutdown(); err != nil {
+		log.Printf("Error shutting down: %v", err)
+	}
+	log.Println("Server was gracefully shut down")
 }
